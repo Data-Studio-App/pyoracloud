b6d69a629bf219b57ead83e7088d2ac4290e2412
Vinod Ronold Aravind Jose <vinodronold@gmail.com>
2021-08-30 08:34:44 -0400

    formatting

--- pyoracloud/__init__.py
+++ pyoracloud/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for pyoracloud."""
 
 __author__ = """Vinod Ronold Aravind Jose"""
-__email__ = 'vinodronold@gmail.com'
-__version__ = '0.1.0'
+__email__ = "vinodronold@gmail.com"
+__version__ = "0.1.0"
